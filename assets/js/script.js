4bbcb38d988fc724f7c6f347ae7a50002f2b95cb
Azer444 <azerrh@code.edu.az>
2022-08-18 04:52:39 +0400

    Javascript

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+//alert("Welcome Cavid Bashirov Teacher");
+//console.log("Salam P130");
+//var name="Azer";
+//var surname = "Humbetov";
+//console.log(name + surname);
+
+
+
+//let name = "Azer";
+//let Surname = "Humbetov";
+//console.log(name + " " + Surname);
+
+
+
+// let age =21;
+// let surname = "Humbetov";
+// let isMarried = true;
+// let adress = null;
+// let count;
+// let person {
+//     name = "Azer",
+//     studentSurname = "Humbetov"
+
+// }
+// console.log(isMarried);
+// console.log(count);
+
+
+let 
